94d3df660b5a051e6035e06e3e4034b5f8457e77
vuongminh1907 <vuongminhkhoi4@gmail.com>
2024-11-20 08:18:11 +0700

    update downloadmodel.py

--- downloadmodel.py
+++ downloadmodel.py
@@ -1,5 +1,5 @@
 from huggingface_hub import hf_hub_download, snapshot_download
-import gdown
+import shutil
 import os
 
 
@@ -22,9 +22,13 @@ hf_hub_download(
 
 os.makedirs("../models/insightface/", exist_ok=True)
 
+os.makedirs("../models/insightface/", exist_ok=True)
+
 snapshot_download(
     repo_id="vuongminhkhoi4/antelopev2",
     cache_dir ="models",
+    repo_type ="model",
     local_dir="../models/insightface",
 )
-
+if os.path.exists("./models") and os.path.isdir("./models"):
+    shutil.rmtree("./models")
